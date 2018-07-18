564782f4d67147d80469f2aef73a4d6c692181a9
Emma Davis <emmadavis01@gmail.com>
2018-07-18 16:54:35 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,6 +1,16 @@
-require_relative 'config/environment'
+class ApplicationController < Sinatra::Base
 
-class App < Sinatra::Base
+  configure do
+  	set :views, "app/views"
+  	set :public_dir, "public"
+  end
 
-
+  get "/" do
+  	erb :index
+  end
+    post "/" do 
+     @the_lane = get_lane
+     @the_user=params[:user]
+     return erb :results
+   end
 end
---
+++ models/models.rb
@@ -0,0 +1,6 @@
+ def get_lane
+    lane_array = ["1", "2","3", "4", "5"]
+    return lane_array.sample
+  end
+  
+  puts get_lane
